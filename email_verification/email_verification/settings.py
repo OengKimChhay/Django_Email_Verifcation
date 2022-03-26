cce2c55ddcb3db3e92bba90c3f0a5966aea5a26a
Oengkimchhay <chihe333@gmail.com>
2022-03-26 16:17:04 +0700

    2

--- email_verification/email_verification/settings.py
+++ email_verification/email_verification/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '09(9g-i5qsz4v^f7wcej%t4i6&e75dcy#b*_*pql4u+c(x7k^d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
